f079f015ed85e8695f2d70a1fadd970fd81459c2
Shane Dharan <ShaneDharan@gmail.com>
2015-12-27 04:38:24 -0500

    changed remoteSecurity to class

--- python_ext/remoteSecurity.py
+++ python_ext/remoteSecurity.py
@@ -3,90 +3,99 @@ from imagePro import imageDetection
 import common.dateTime as formatTime
 import common.var as var
 
-def snapShot(cam):
-    cam.readCamera()
-    cam.showFrame1()
-    #cam.showFrame2()
+class remote(object):
 
-def setImages(cam, control):
-    image1 = cam.getGreyFrame1().copy()
-    image2 = cam.getGreyFrame2().copy()
-    control.setImage1(image1)
-    control.setImage2(image2)
-
-def showExtra(cam, control):
-    control.showThresholdImage()
-    control.showDifferenceImage()
-
-def closeExtra(cam, control):
-    cam.closeAll()
-
-def detectMotion(control):
-    motionDetected = False
-    control.thresholdCalc()
-    motionDetected = control.detectMotion()
-    time = getTime()
-
-    #replace with text and dropbox api later
-    if motionDetected == True:
-        print 'MOTION DETECTED at %s!!' % (time)
-
-    else:
-        print "NO MOTION"
-
-def getTime():
-    time = formatTime.getTime()
-    return time
-
-def getFileDateTime():
-    time = formatTime.getFileTime()
-    return time
-
-def userInput(key):
-    inputChar = chr(key & 255)
-    return inputChar
-
-def debugControl(debug, securityCam, imageControl):
-    debug = not(debug)
-    if debug == False:
-        closeExtra(securityCam, imageControl)
-    return debug
-
-def main():
     debug = False
-    securityCam =videoFeed()
-    imageControl = imageDetection()
+    cam = 0
+    control = 0
 
-    while(True):
-        #capture frame by fraame
-        snapShot(securityCam)
+    def __init__(self):
+        self.debug = False
+        self.cam = videoFeed()
+        self.control = imageDetection()
 
-        #transfer data and calculate threshold images
-        setImages(securityCam, imageControl)
+    def snapShot(self):
+        self.cam.readCamera()
+        self.cam.showFrame1()
 
-        #search for motion within camera range
-        detectMotion(imageControl)
+    def setImages(self):
+        image1 = self.cam.getGreyFrame1().copy()
+        image2 = self.cam.getGreyFrame2().copy()
+        self.control.setImage1(image1)
+        self.control.setImage2(image2)
 
-        #debug mode for program
-        if debug == True:
-            showExtra(securityCam, imageControl)
+    def showExtra(self):
+        self.control.showThresholdImage()
+        self.control.showDifferenceImage()
 
-        #inputs from user
-        securityCam.checkInput()
-        key = securityCam.getKey()
-        options = userInput(key)
+    def closeExtra(self):
+        self.cam.closeAll()
 
-        #check if input matches expected inputs
-        #quit the program
-        if options == var.QUIT:
-            break
+    def detectMotion(self):
+        motionDetected = False
+        self.control.thresholdCalc()
+        motionDetected = self.control.detectMotion()
+        time = self.getTime()
 
-        #check if debug mode
-        elif options == var.DEBUG:
-            debug = debugControl(debug, securityCam, imageControl)
+        #replace with text and dropbox api later
+        if motionDetected == True:
+            print 'MOTION DETECTED at %s!!' % (time)
 
-    #releases camera from the program
-    securityCam.close()
+        else:
+            print "NO MOTION"
+
+    def getTime(self):
+        time = formatTime.getTime()
+        return time
+
+    def getFileDateTime(self):
+        time = formatTime.getFileTime()
+        return time
+
+    def userInput(self, key):
+        inputChar = chr(key & 255)
+        return inputChar
+
+    def debugControl(self, debug):
+        debug = not(debug)
+        if debug == False:
+            self.closeExtra()
+        return debug
+
+    def main(self):
+        debug = False
+
+        while(True):
+            #capture frame by fraame
+            self.snapShot()
+
+            #transfer data and calculate threshold images
+            self.setImages()
+
+            #search for motion within camera range
+            self.detectMotion()
+
+            #debug mode for program
+            if debug == True:
+                self.showExtra()
+
+            #inputs from user
+            self.cam.checkInput()
+            key = self.cam.getKey()
+            options = self.userInput(key)
+
+            #check if input matches expected inputs
+            #quit the program
+            if options == var.QUIT:
+                break
+
+            #check if debug mode
+            elif options == var.DEBUG:
+                debug = self.debugControl(debug)
+
+        #releases camera from the program
+        self.cam.close()
 
 if __name__ == "__main__":
-    main()
+    security = remote()
+    security.main()
