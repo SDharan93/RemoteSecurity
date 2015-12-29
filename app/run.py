3e656d4eb0da07d0fb8666c2b19ef4c04ea71688
Shane Dharan <ShaneDharan@gmail.com>
2015-12-28 21:43:29 -0500

    moved run file

---
+++ app/run.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, redirect
+import twilio.twiml as twiml
+
+app = Flask(__name__)
+
+@app.route("/", method=['GET', 'POST'])
+
+def response():
+    fromMessage = request.form['Body']
+    toMessage = 'Hey %s, how are you today?' % (message)
+    print toMessage
+
+    response = twiml.Response()
+    response.message(toMessage)
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(debug=True)
